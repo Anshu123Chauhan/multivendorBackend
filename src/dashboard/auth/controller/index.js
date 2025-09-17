c228fd81acc3bbc5d45de79c37056eae7dcc1381
yogesh sharma <yogesh.sharma@ens.enterprises>
2025-09-17 10:42:41 +0530

    fix user auth and login

--- src/dashboard/auth/controller/index.js
+++ src/dashboard/auth/controller/index.js
@@ -20,7 +20,10 @@ export const login = async (req, res) => {
     if (!user) return res.status(400).json({ error: 'Invalid credentials' });
     const ok = await user.comparePassword(password);
     if (!ok) return res.status(400).json({ error: 'Invalid credentials' });
-    const token = signToken(user);
+    user = user.toObject();
+    delete user.password;
+    user.userType = loginType;
+    const token = signToken(user);    
     res.json({ success: true, token, message: "login successfully" });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
