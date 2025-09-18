caac60a0c26da3082def88b5a6fd9051e0626452
Srishti Sharma <106140991+shri5445@users.noreply.github.com>
2025-09-18 15:51:39 +0530

    single variant add update restore and delete

--- src/dashboard/product/routes.js
+++ src/dashboard/product/routes.js
@@ -5,6 +5,11 @@ import {
   getProduct,
   updateProduct,
   deleteProduct,
+  restoreProduct,
+  addVariant,
+  updateVariant,
+  deleteVariant,
+  restoreVariant
 } from "./controller/index.js";
 
 const router = express.Router();
@@ -14,5 +19,12 @@ router.get("/", getProducts);
 router.get("/:id", getProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
+router.patch("/:id/restore", restoreProduct);
+
+
+router.post("/:id/variants/", addVariant);
+router.put("/:id/variants/:variantId", updateVariant);
+router.delete("/:id/variants/:variantId", deleteVariant);
+router.patch("/:id/:variantId/restore", restoreVariant);
 
 export default router;
