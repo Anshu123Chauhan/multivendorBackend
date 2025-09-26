61df593ca4e1702c23041ed68b5390b535c8bb45
Srishti Sharma <106140991+shri5445@users.noreply.github.com>
2025-09-26 15:26:49 +0530

    payment setting route add

--- src/dashboard/paygateway/routes.js
+++ src/dashboard/paygateway/routes.js
@@ -1,6 +1,11 @@
 import express from 'express';
-import {createpaymentsetting,getpaymentsettings,getpaymentsettingById} from './controller/index.js'
+import {createPaymentSetting,getPaymentSettings,getPaymentSettingById,updatePaymentSetting} from './controller/index.js'
 const router = express.Router();
 
-router.post('/',createpaymentsetting)
+router.post('/',createPaymentSetting)
+router.get('/',getPaymentSettings)
+router.get('/:id',getPaymentSettingById)
+router.put('/:id',updatePaymentSetting)
+
+export default router
 
