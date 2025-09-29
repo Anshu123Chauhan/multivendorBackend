f5ad966941320cdfa01051f10528fd0e473ae40c
yogesh sharma <yogesh.sharma@ens.enterprises>
2025-09-29 13:39:08 +0530

    fix address crud api

--- src/ecommerce/customer/routes.js
+++ src/ecommerce/customer/routes.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import { authenticate } from '../auth/index.js';
 import { customerRegister, customerDetail, login, sentOtp, updateForgetPassword, customerPasswordUpdate, verifyOtp,
-    addAddress, updateAddress, removeAddress } from './controller/index.js';
+    addAddress, getAddress, getAddressList, updateAddress, removeAddress } from './controller/index.js';
 
 const router = express.Router();
 
 router.post('/auth/login', login);
 router.post('/register', customerRegister);
-router.post('/detail/:id', authenticate, customerDetail);
+router.get('/detail/:id', authenticate, customerDetail);
 router.post('/update-password', authenticate, customerPasswordUpdate);
 router.post('/forget-password', updateForgetPassword);
 router.post('/send-otp', sentOtp);
 router.post('/verify-otp', verifyOtp);
 router.post('/address/add', authenticate, addAddress);
+router.get('/address/list', authenticate, getAddressList);
+router.get('/address/get/:id', authenticate, getAddress);
 router.put('/address/update/:id', authenticate, updateAddress);
 router.delete('/address/remove/:id', authenticate, removeAddress);
 
--- src/models/admin.js
+++ src/models/admin.js
@@ -5,7 +5,7 @@ const adminSchema = new mongoose.Schema(
   {
     id: { type: Number, unique: true, sparse: true },
 
-    username: { type: String, required: true, unique: true, trim: true },
+    username: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
 
