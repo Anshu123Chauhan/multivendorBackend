5deafdee002e45f805c48bffb09947c3e5a601ad
yogesh sharma <yogesh.sharma@ens.enterprises>
2025-10-09 13:36:31 +0530

    update model wishlist implement cart to wishlist and wishlist to cart api optimize code of cart crud

--- src/ecommerce/cart/routes.js
+++ src/ecommerce/cart/routes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { addToCart, getCart, updateCartItem, removeCartItem, clearCart,
+import { addToCart, getCart, updateCartItem, removeCartItem, clearCart, wishlistToCart
 } from "./controller/index.js";
+import { authenticate } from "../auth/index.js";
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.get("/list", getCart);
 router.put("/update", updateCartItem);
 router.delete("/remove", removeCartItem);
 router.delete("/clear", clearCart);
+router.post("/wishlist-to-cart", authenticate, wishlistToCart);
 
 export default router;
--- src/ecommerce/wishlist/routes.js
+++ src/ecommerce/wishlist/routes.js
@@ -3,7 +3,8 @@ import {
   getWishlist,
   addToWishlist,
   removeFromWishlist,
-  clearWishlist
+  clearWishlist,
+  cartToWishlist
 } from "./controller/index.js";
 import { authenticate } from "../auth/index.js";
 
@@ -13,5 +14,6 @@ router.get("/", authenticate, getWishlist);
 router.post("/add", authenticate, addToWishlist);
 router.post("/remove", authenticate, removeFromWishlist);
 router.delete("/clear", authenticate, clearWishlist);
+router.post("/cart-to-wishlist", authenticate, cartToWishlist);
 
 export default router;
