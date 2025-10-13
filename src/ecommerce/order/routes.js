2cfb2a869214706bc7024f2b861dc989d918877a
yogesh sharma <yogesh.sharma@ens.enterprises>
2025-10-13 16:10:22 +0530

    create single order api

--- src/ecommerce/order/routes.js
+++ src/ecommerce/order/routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { placeOrder, getCustomerOrders } from "./controller/index.js";
+import { placeOrder, getCustomerOrders, customerOrder } from "./controller/index.js";
 
 
 const router = express.Router();
 
 router.post("/place", placeOrder);
 router.get("/list",getCustomerOrders)
+router.get("/:id",customerOrder)
 
 export default router;
